c02ec0d102c9251287f186def3d1e3acd054e58d
Davi10995 <davide.corda95@gmail.com>
2020-05-07 11:52:44 +0200

    custom button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'primoTask';
+  // For button
+  type: string ;
+  name: string;
+  class: string;
+  label: string;
+
+
+  getName(event){
+    this.name = event.target.value;
+  }
+  getType(event){
+    this.type = event.target.value;
+  }
+  getLabel(event){
+    this.label = event.target.value;
+  }
+  getClass(event){
+    this.class = event.target.value;
+  }
 }
+
